5766bc51e4140c1bda6bc1a7461c8956664ae295
dantedxwolf <teacher.martin@gmail.com>
2020-12-21 00:59:50 +0600

    User Events on trigger added

--- dist/src.f10117fe.js
+++ dist/src.f10117fe.js
@@ -117,9 +117,94 @@ parcelRequire = (function (modules, cache, entry, globalName) {
   }
 
   return newRequire;
-})({"src/index.ts":[function(require,module,exports) {
-console.log('This is typescript project');
-},{}],"C:/Users/Dell/AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+})({"src/models/User.ts":[function(require,module,exports) {
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.User = void 0; // following is a User class
+
+var User =
+/** @class */
+function () {
+  function User(data) {
+    this.data = data;
+    this.events = {};
+  }
+
+  User.prototype.get = function (propName) {
+    return this.data[propName];
+  };
+
+  User.prototype.set = function (update) {
+    Object.assign(this.data, update);
+  };
+
+  User.prototype.on = function (eventName, callback) {
+    var handlers = this.events[eventName] || [];
+    handlers.push(callback);
+    this.events[eventName] = handlers;
+  };
+
+  User.prototype.trigger = function (eventName) {
+    var handlers = this.events[eventName];
+
+    if (!handlers || handlers.length === 0) {
+      return;
+    }
+
+    handlers.forEach(function (callback) {
+      callback();
+    });
+  };
+
+  return User;
+}();
+
+exports.User = User;
+},{}],"src/index.ts":[function(require,module,exports) {
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var User_1 = require("./models/User");
+
+var shadman = new User_1.User({
+  name: 'shadman',
+  age: 20
+});
+console.log(shadman.get('name'));
+console.log(shadman.get('age'));
+shadman.set({
+  name: 'piyal',
+  age: 100
+});
+console.log(shadman); // {name: 'piyal', age: 100}
+
+shadman.on('change', function () {
+  console.log("change #1");
+});
+shadman.on('change', function () {
+  console.log("change #2");
+});
+shadman.on('change', function () {
+  console.log("change #3");
+});
+shadman.on('save', function () {
+  console.log("save #1");
+});
+shadman.on('save', function () {
+  console.log("save #2");
+});
+shadman.on('save', function () {
+  console.log("save #3");
+});
+shadman.trigger('change');
+shadman.trigger('save');
+},{"./models/User":"src/models/User.ts"}],"C:/Users/Dell/AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -147,7 +232,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63638" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52857" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log('This is typescript project');
+import { User } from './models/User';
+
+let shadman = new User({ name: 'shadman', age: 20 });
+console.log(shadman.get('name'));
+console.log(shadman.get('age'));
+shadman.set({ name: 'piyal', age: 100 });
+console.log(shadman);
+// {name: 'piyal', age: 100}
+
+shadman.on('change', () => { console.log("change #1") });
+shadman.on('change', () => { console.log("change #2") });
+shadman.on('change', () => { console.log("change #3") });
+
+shadman.on('save', () => { console.log("save #1") });
+shadman.on('save', () => { console.log("save #2") });
+shadman.on('save', () => { console.log("save #3") });
+
+shadman.trigger('change');
+shadman.trigger('save');
