2cb3b266e5b8099c0e773d801727f462029c85e0
dantedxwolf <teacher.martin@gmail.com>
2020-12-21 05:33:13 +0600

    Normally User class implemented

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,9 +1,11 @@
 interface UserPropsMain{
+  id?: number;
   name: string;
   age: number;
 }
 
 interface UserPropsSet{
+  id?: number;
   name?: string;
   age?: number;
 }
@@ -12,18 +14,24 @@ type CallBack = () => void; //  A CallBack function type alias which takes no ar
 
 
 interface UserClassInterface{
+  events: { [key: string]: CallBack[] }
   get(propsName: string): (string | number);
   set(update: UserPropsSet): void;
   on(eventName: string, callback: CallBack): void;
-  trigger(eventName: string): void
+  trigger(eventName: string): void;
+  fetch(): void;
+  save(): void;
 }
 
 // following is a User class
 
+import axios, { AxiosResponse } from 'axios';
+
 export class User implements UserClassInterface{
   events: { [key: string]: CallBack[] } = {};
 
-  constructor(private data: UserPropsMain) { }
+  constructor(private data: UserPropsMain) { 
+  }
   
   get(propName: string): (string | number) {
     return this.data[propName];
@@ -47,5 +55,24 @@ export class User implements UserClassInterface{
     handlers.forEach(callback => {
       callback();
     })
+  }
+
+  fetch(): void{
+    axios.get(`http://localhost:3000/users/`)
+      .then((response: AxiosResponse): void => {
+        console.log(response.data);
+      })
+      .catch(err => console.log(err));
+  }
+
+  save(): void{
+    if (!this.data.id) {
+      axios.post(`http://localhost:3000/users/`, this.data)
+        .then((response: AxiosResponse): void => {
+          this.data.id = response.data.id;
+        });
+    } else if(this.data.id) {
+      axios.put(`http://localhost:3000/users/${this.get('id')}`, this.data);
+    }
   }
 }
